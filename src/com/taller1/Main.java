8e09a76d9b4e6c297d9e382c208f628691ba1c47
Denis <denis.mj@outlook.com>
2018-03-18 20:10:25 -0300

    Agregado un mensaje de bienvenida

--- src/com/taller1/Main.java
+++ src/com/taller1/Main.java
@@ -6,6 +6,8 @@ package com.taller1;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Bienvenido al Tragamonedas de Programacion I");
+        System.out.println("============================================");
         Tragamonedas tragamonedas = new Tragamonedas();
         tragamonedas.play();
     }
